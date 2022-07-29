a3cba9ab74d707863db269faa679dad81b38ac26
Luciano Silva <93166470+LucianoSilva2020@users.noreply.github.com>
2022-07-29 00:17:19 -0300

    ..

--- ProgressaoAritmetica.py
+++ ProgressaoAritmetica.py
@@ -1,1 +1,6 @@
-primeiro = int(input('Primeiro termo N:'))
+primeiro = int(input('Primeiro termo Nº:'))
+razao = int(input('Razão Nº:'))
+decimo = primeiro + (10 - 1) * razao
+for var in range (primeiro, decimo + razao, razao):
+    print(f'{var}', end='→ ')
+print('ACABOU')    
